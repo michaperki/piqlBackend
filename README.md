d29d8dd1fff12739cf2a8bd3867123fe2f1f5b53
Michael Perkins <mperkins1995@gmail.com>
2023-09-06 02:20:13 -0500

    binding port

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 ## Helpful Links
 
 - [Database migration tip](https://stackoverflow.com/a/73163909/19620151)
+- [Heroku PostgreSQL Bug (and work around)](https://stackoverflow.com/questions/62688256/sqlalchemy-exc-nosuchmoduleerror-cant-load-plugin-sqlalchemy-dialectspostgre)
--- run.py
+++ run.py
@@ -1,6 +1,11 @@
+import os
 from app import create_app
 
 app = create_app()
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    # Use Heroku's provided PORT environment variable or default to 5000
+    port = int(os.environ.get('PORT', 5000))
+
+    # Run the Flask app on 0.0.0.0 to bind to all available network interfaces
+    app.run(host='0.0.0.0', port=port)
