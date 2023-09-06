069347b9a5a569776fff4f183671d5e49d537d40
Michael Perkins <mperkins1995@gmail.com>
2023-09-06 01:51:29 -0500

    Hardcode Heroku DB in app/__init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -7,7 +7,7 @@ db = SQLAlchemy()
 
 def create_app():
     app = Flask(__name__)
-    app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///dev.db')
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://upsbiqnupwwtng:f1a635fcc73114b225997d2e62eaedf9a59a0eff523909d4af657ab6c04cda89@ec2-44-215-40-87.compute-1.amazonaws.com:5432/d9ees8p5hg2pn7'
 
     # Initialize the database with the app
     db.init_app(app)
