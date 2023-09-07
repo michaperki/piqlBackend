dba1a472aa58ebbb0c3bb5df51b061c9780c75e9
Michael Perkins <mperkins1995@gmail.com>
2023-09-06 21:18:10 -0500

    bug fixing user auth

--- app/models.py
+++ app/models.py
@@ -14,19 +14,19 @@ class Item(db.Model):
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(255), nullable=False)
+    password_hash = db.Column(db.String(255), nullable=False)  # Rename 'password' to 'password_hash'
 
     def __init__(self, email, password):
         self.email = email
-        self.set_password(password)  # Use the set_password method to hash and store the password.
+        self.set_password(password)
 
     def set_password(self, password):
         # Hash and store the password as a string
-        self.password = hashpw(password.encode('utf-8'), gensalt()).decode('utf-8')
+        self.password_hash = hashpw(password.encode('utf-8'), gensalt()).decode('utf-8')
 
     def check_password(self, password):
         # Use bcrypt to check the password
-        return checkpw(password.encode('utf-8'), self.password.encode('utf-8'))
+        return checkpw(password.encode('utf-8'), self.password_hash.encode('utf-8'))
 
     def __repr__(self):
         return f'<User {self.email}>'
