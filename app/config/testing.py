dba90e59935e15d91e50ada69e43de774b2a33ef
Michael Perkins <mperkins1995@gmail.com>
2023-09-06 21:11:17 -0500

    bug fixing secret keys

--- app/config/testing.py
+++ app/config/testing.py
@@ -4,5 +4,5 @@ class TestingConfig:
     TESTING = True
     SQLALCHEMY_DATABASE_URI = "sqlite:///test.db"  # Use SQLite for testing
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    JWT_SECRET_KEY = 'BRUCE'
-    SECRET_KEY = 'BLU'
+    JWT_SECRET_KEY = 'mysecret'
+    SECRET_KEY = 'mysecret'
