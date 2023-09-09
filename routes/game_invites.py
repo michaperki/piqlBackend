f84c31d1ec7b9c3ab934e679df01692bea00f71b
Michael Perkins <mperkins1995@gmail.com>
2023-09-09 01:22:40 -0500

    bug fix accept invite, APT

--- routes/game_invites.py
+++ routes/game_invites.py
@@ -35,23 +35,29 @@ def get_game_invites():
 # Route to accept a game invite
 @game_invites_bp.route('/game-invites/accept/<int:invite_id>', methods=['POST'])
 @jwt_required()
-def accept_game_invite(invite_id):
-    current_user_id = get_jwt_identity()
+def accept_game_invite(game_id):
+    # Get the current user
+    current_user = get_jwt_identity()
+    if current_user is None:
+        return jsonify({"msg": "User not found"}), 404
 
-    # Query the game_invites table to find the game invite by ID
-    game_invite = Game.query.filter(Game.invites.any(id=current_user_id), Game.id == invite_id).first()
+    # Retrieve the game invite
+    game_invite = Game.query.get(game_id)
 
-    if not game_invite:
-        return jsonify({"error": "Game invite not found or not accessible"}), 404
+    if game_invite is None:
+        return jsonify({"msg": "Game invite not found"}), 404
 
-    # Remove the invite from the game_invites table
-    game_invite.invites.remove(current_user_id)
+    # Check if the current user is invited to the game
+    if current_user not in game_invite.invites:
+        return jsonify({"msg": "User is not invited to this game"}), 403
 
-    # Add the current user to the game's players
-    game_invite.players.append(current_user_id)
+    # Remove the user from the invites list
+    game_invite.invites.remove(current_user)
+
+    # Commit the changes to the database
     db.session.commit()
 
-    return jsonify({"message": "Game invite accepted"}), 200
+    return jsonify({"msg": "Game invite accepted"}), 200
 
 # Route to reject a game invite
 @game_invites_bp.route('/game-invites/reject/<int:invite_id>', methods=['POST'])
